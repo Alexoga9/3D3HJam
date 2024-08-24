6e6d084d50e06abe56527517206c0ee2fc62b36c
Alexoga9 <alexalga32@gmail.com>
2024-08-24 08:55:20 -0400

    Menú principal, esquema

---
+++ Scripts/menu_principal.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_begin_pressed():
+	get_tree().change_scene_to_file("Escena")
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
