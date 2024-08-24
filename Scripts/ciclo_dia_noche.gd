75cb62df8cc75d4a370f026ccd94d1892152f85a
Alexoga9 <alexalga32@gmail.com>
2024-08-23 20:22:56 -0400

    Pequeños ajustes iniciales

---
+++ Scripts/ciclo_dia_noche.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@export var ciclo_diurno = WorldEnvironment
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _ciclo_dia_noche():
+	pass # Replace with function body.
