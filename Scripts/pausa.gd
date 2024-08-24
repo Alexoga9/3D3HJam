d00c19d63e79e3b969d76336bed5d06249a94334
Alexoga9 <alexalga32@gmail.com>
2024-08-24 12:12:33 -0400

    Camara
    Ajuste de la camara y varios espacios borrados del codigo

--- Scripts/pausa.gd
+++ Scripts/pausa.gd
@@ -1,26 +1,20 @@
 extends Control
 
 var pausado = true
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if  Input.is_action_just_pressed("pausa"):
 		PausaMenu()
-	
+
 
 func PausaMenu():
 	if !pausado:
 		self.hide()
 		Engine.time_scale = 1
-	
 	else:
 		self.show()
 		Engine.time_scale = 0
-	
 	pausado = !pausado
 
 
